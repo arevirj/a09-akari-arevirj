1bd25e9d2987dd6179c2c8547ce88a1ddc9dd419
Justin Rivera <jrivera1@unc.edu>
2023-04-27 19:27:04 -0400

    It looks good

--- src/main/java/com/comp301/a09akari/view/PuzzleView.java
+++ src/main/java/com/comp301/a09akari/view/PuzzleView.java
@@ -7,6 +7,8 @@ import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class PuzzleView implements FXComponent {
@@ -23,34 +25,34 @@ public class PuzzleView implements FXComponent {
     layout.getStyleClass().add("board");
     int columns = currentPuzzle.getWidth();
     int rows = currentPuzzle.getHeight();
-    Button corridor = new Button();
-    layout.getChildren().add(corridor);
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < columns; j++) {
         if (currentPuzzle.getCellType(i, j) == CellType.CORRIDOR) {
-          Button corridorButton = new Button();
+          Button corridorButton = new Button("  ");
           int finalI = i;
           int finalJ = j;
           corridorButton.setOnAction(
               (ActionEvent event) -> {
                 controller.clickCell(finalI, finalJ);
               });
+
+          corridorButton.getStyleClass().add("corridor");
           layout.add(corridorButton, j, i);
           if (controller.isLit(i, j)) {
             if (controller.isLamp(i, j)) {
               if (controller.isLampIllegal(i, j)) {
-                Label illegal = new Label("X");
-                illegal.getStyleClass().add("lit");
-                layout.add(illegal, j, i);
+                corridorButton.setText("X");
+                corridorButton.getStyleClass().add("lit");
               } else {
-                Label lit = new Label("L");
-                lit.getStyleClass().add("lit");
-                layout.add(lit, j, i);
+                ImageView img = new ImageView(new Image("light-bulb.png"));
+                img.setFitHeight(8.0);
+                img.setFitWidth(8.0);
+                corridorButton.setText("");
+                corridorButton.setGraphic(img);
+                corridorButton.getStyleClass().add("lit");
               }
             } else {
-              Label lit = new Label("   ");
-              lit.getStyleClass().add("lit");
-              layout.add(lit, j, i);
+              corridorButton.getStyleClass().add("lit");
             }
           }
 
@@ -58,6 +60,8 @@ public class PuzzleView implements FXComponent {
           Label tile = new Label(String.valueOf(controller.getActivePuzzle().getClue(i, j)));
           if (controller.isClueSatisfied(i, j)) {
             tile.getStyleClass().add("solved-clue");
+          } else {
+            tile.getStyleClass().add("clue");
           }
           layout.add(tile, j, i);
         } else {
